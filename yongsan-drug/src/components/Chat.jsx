feb887baa2d7452aadd1c58abccb0cb0d8cff267
unknown <yjs12180825@gmail.com>
2023-08-17 18:12:50 +0900

    commit

--- yongsan-drug/src/components/Chat.jsx
+++ yongsan-drug/src/components/Chat.jsx
@@ -1,6 +1,7 @@
 import { Client } from "./ClientChat";
 import { Bot } from "./BotChat";
 
+// eslint-disable-next-line react/prop-types
 const Chat = ({ sender, text }) => {
   return <>{sender ? <Bot text={text} /> : <Client text={text} />} </>;
 };
